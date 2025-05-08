a0de3adc83ae9b4521c182bde279d7f97bca4ac1
dhirajdhande19 <dhirajdhande2@gmail.com>
2025-05-08 22:32:47 +0530

    made update

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 let form = document.querySelector("form");
+
+let ul = document.querySelector("ul");
 let name = document.querySelector("#name");
 let title = document.querySelector("#title");
 let price = document.querySelector("#price");
@@ -10,12 +12,30 @@ let delete_btn = document.querySelector("#delete_btn");
 
 
 
-
 add_btn.addEventListener("click", (e) => {
-    e.preventDefault(); 
-    console.log(name.value);
-    console.log(title.value);
-    console.log(price.value);
-    console.log(date.value);
-    form.reset();
+    e.preventDefault();
+
+    // let item1 = document.createElement("p");
+    // item1.innerText = name.value;
+    // let item2 = document.createElement("p");
+    // item2.innerText = title.value;
+    // let item3 = document.createElement("p");
+    // item3.innerText = price.value;
+    // let item4 = document.createElement("p");
+    // item4.innerText = date.value;
+
+
+    let subcard = document.createElement("div");
+    subcard.classList.add("subcard");
+    let values = [name.value, title.value, price.value, date.value];
+    for(val of values) {
+        let p = document.createElement("p");
+        p.innerText = val;
+        subcard.appendChild(p);
+    }
+
+    ul.appendChild(subcard);
+
+    form.reset(); 
 });
+
